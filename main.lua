ba1e3565ab41c6f0d36ca79985c050fa8b2edbd5
trabitboy <trabitboy@gmail.com>
2019-04-07 22:22:21 +0200

    test brush

--- main.lua
+++ main.lua
@@ -4,7 +4,27 @@ require('loadfilter')
 
 prjfld="project/"
 
+
+--TODO faking for now
+function createBrush(d)
+	mine=love.image.newImageData(d,d)
+	for j=0,d-1,1 
+	do
+		for i=0,d-1,1
+		do
+			mine:setPixel(i,j,1.0,1.0,1.0,1.0)
+		
+		end
+	end
+	
+	return mine
+end
+
+
 function love.load()
+
+	mybrush=love.graphics.newImage(createBrush(16))
+	
 
 	frames={}
 	files=love.filesystem.getDirectoryItems(prjfld)
@@ -22,5 +42,13 @@ end
 
 function love.draw()
 	love.graphics.print('zaz2nim',0,0)
+	love.graphics.setColor(1.0,1.0,1.0,0.5)
 	love.graphics.draw(frames[1].pic,0,0)
+	love.graphics.setColor(1.0,1.0,1.0,0.5)
+	love.graphics.draw(frames[3].pic,0,0)
+	
+	love.graphics.setColor(1.0,1.0,1.0,1.0)
+	love.graphics.draw(frames[2].pic,0,0)
+	
+	love.graphics.draw(mybrush,0,0)
 end
