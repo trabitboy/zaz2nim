41f933d49e0a92949d922a5d1320e2119273a0ca
trabitboy <trabitboy>
2020-06-10 11:30:47 +0200

    wip

--- storyboard.lua
+++ storyboard.lua
@@ -3,13 +3,48 @@ local widgets={}
 
 local stamps={}
 
+local boardWidth=math.floor(uiw*0.8)
+
+local defaultStampW= math.floor( boardWidth/32 )
+
+local defaultStampH= math.floor(defaultStampW*conf.cvsw/conf.cvsh)
+
+--when the algorithm below 
+function allocateStamp()
+
+
+end
+
 
 --we build a representation of the stamps to be displayed,
 --taking into account the width to fill,
 --and the height of the screen
 function maintainStamps()
 	 --we start at line 0
-	 --TODO
+	 --TODO handle width
+	 --TODO start at current idx
+	 local tx=0
+	 local ty=0
+
+	 for i=1,maxframe
+	 do
+
+		--TODO allocation code duplicated mmmm
+		if tx+(defaultStampW*frames[i].tc)<boardWidth then
+		   --we use the space 
+		   
+		   
+
+		else
+			ty=ty+defaultStampH
+
+			if ty>uih then return end
+
+			--then we use the space
+
+		end
+	 end
+
 
 end
 
