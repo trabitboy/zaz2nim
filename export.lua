fc66b47216c395068d8d27a2cd78700a2800ccbf
trabitboy <trabitboy@gmail.com>
2020-06-26 12:05:04 +0200

    Update export.lua

--- export.lua
+++ export.lua
@@ -2,3 +2,5 @@
 --duplicate frames for timecodes > 1
 --compose ffmpeg scripts until separate exporter app is available
 
+--design it as a batch with feedback ( does one, updates status, status can be rendered,
+-- job called again )
